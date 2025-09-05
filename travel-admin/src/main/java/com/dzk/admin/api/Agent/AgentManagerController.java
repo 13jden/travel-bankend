43eb38b0c3b8d87a89dfc10f69eca54ac9198602
13jdden <13jdenden@gmail.com>
2025-09-05 16:41:55 +0800

    fix:服务器CI/CD测试

--- travel-admin/src/main/java/com/dzk/admin/api/Agent/AgentManagerController.java
+++ travel-admin/src/main/java/com/dzk/admin/api/Agent/AgentManagerController.java
@@ -1,7 +1,12 @@
 package com.dzk.admin.api.Agent;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AgentManagerController {
+    @GetMapping("/test")
+    public String test() {
+        return "docker配置测试🚀🚀🚀";
+    }
 }
